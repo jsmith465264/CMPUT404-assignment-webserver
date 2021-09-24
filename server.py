b7d5174a11562318e1dbc9d0a5d11d5e564cb512
jsmith465264 <43418313+jsmith465264@users.noreply.github.com>
2021-09-23 22:54:56 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 #  coding: utf-8 
 import socketserver
+import email
+from io import StringIO
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
+# Changes made by Joshua Smith
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -30,9 +35,92 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        self.data = self.request.recv(1024).strip().decode("utf-8") 
+        print("Got a request of: %s\n" % self.data)
+        requestLine, headers = self.data.split("\r\n", 1)
+        message = email.message_from_file(StringIO(headers))
+        headers = dict(message.items())
+        method, path, typ = requestLine.split(' ', 2)
+        if method != 'GET':
+            r = "HTTP/1.1 405 OK\nContent-Type: text/plain\nContent-Length: 0\r\n"
+        else:
+            r = self.getFile(path)
+        print(bytearray(r,'utf-8'))
+        self.request.sendall(bytearray(r,'utf-8'))
+        
+    def getFile(self, requestPath):
+        pathParts = requestPath.split("/")
+        if len(pathParts) < 2:
+            return self.ret404()
+        i = 1
+        path = os.getcwd()+ "/www/"
+        while i < len(pathParts)-1:
+            if pathParts[i] == "..":
+                return self.ret404()
+            #go to next directory
+            if os.path.exists(path+pathParts[i]):
+                path = path + pathParts[i] +"/"
+                
+                i+=1
+            #if doesn't exist return 404
+            else:
+                return self.ret404()
+        if pathParts[len(pathParts)-1] == '':
+            path = path + "index.html"
+            if os.path.isfile(path):
+                return self.getFileResponseHTML(path)
+            else:
+                return self.ret404()
+        path = path + pathParts[len(pathParts)-1]
+        if os.path.isfile(path): 
+            _ ,mimeType = pathParts[len(pathParts)-1].split(".")
+            try:
+                if mimeType == "html":
+                    return self.getFileResponseHTML(path)
+                elif mimeType == "css":
+                    return self.getFileResponseCSS(path)
+                else:
+                    return self.getFileResponseOther(path)
+            except:
+                pass
+        if os.path.exists(path):
+            path = path + "/index.html"
+            if os.path.isfile(path):
+                return self.getFileResponseHTML301(path)
+            else:
+                return self.ret404()
+        return self.ret404()
+    
+    def getFileResponseHTML(self, path):
+        content = open(path, "r").read()
+        x = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=iso-8859-1\nConnection: close\nContent-Length: 1000\r\n" + content
+        direct, _ = path.rsplit("/", 1)
+        print("direct" + direct)
+        for i in os.listdir(direct):
+            try:
+                _ ,mimeType = i.split(".")
+                if mimeType == "css":
+                    print(i)
+                    x = x.replace(
+                        '<link rel="stylesheet" type="text/css" href="' + i + '">',
+                        "<style>" +(open((direct+"/"+i), "r").read()) + "</style>"
+                    )
+                    print(x)
+            except:
+                pass
+        return x
+    def getFileResponseHTML301(self, path):
+        content = open(path, "r").read()
+        return "HTTP/1.1 301 Moved Permanently\nContent-Type: text/html\nContent-Length: 1000\r\n" + content    
+    def getFileResponseCSS(self, path):
+        content = open(path, "r").read()
+        return "HTTP/1.1 200 OK\nContent-Type: text/css\nContent-Length: 1000\r\n" + content
+    def getFileResponseOther(self, path):
+        content = open(path, "r").read()
+        return "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 0\r\n" + content
+    def ret404(self):
+        return "HTTP/1.1 404 Not Found\nContent-Type: text/plain\nContent-Length: 0\r\n"
+    
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
